a83faa3277c673feb24f1f4f74955faff363b72a
Patryk Kaczmarek <patryk.kaczmarek@wp.eu>
2023-04-24 14:42:49 +0200

    updated build script

--- build_and_distribute_to_firebase.bat
+++ build_and_distribute_to_firebase.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 REM Define the Firebase project ID and distribution group ID
 set FIREBASE_PROJECT_ID=1:43735507988:android:f5f41e8de9650b4dc67301
@@ -10,28 +10,36 @@ set STAGING_GROUP_ID=staging-testers
 set PRODUCTION_GROUP_ID=production-testers
 
 REM Loop through all project flavors and build the APKs
-for %%f in (development staging production) do (
-    echo Building APK for %%f...
-    gradlew.bat assemble%%fDebug
-    REM gradlew.bat assemble%%fRelease
-)
+REM for %%f in (development staging production) do (
+REM     echo Building APK for %%f...
+REM     gradlew.bat assemble%%fDebug
+REM     REM gradlew.bat assemble%%fRelease
+REM )
 
 REM Loop through all built APKs and distribute them to Firebase
 for /R app\build\outputs\apk %%f in (*.apk) do (
     set DISTRIBUTION_GROUP=
-
-    if "%%~nf"=="app-development-debug" (
-        set DISTRIBUTION_GROUP=%DEVELOPMENT_GROUP_ID%
-    ) else if "%%~nf"=="app-staging-debug" (
-        set DISTRIBUTION_GROUP=%STAGING_GROUP_ID%
-    ) else if "%%~nf"=="app-production-debug" (
-        set DISTRIBUTION_GROUP=%PRODUCTION_GROUP_ID%
+    echo "%%~nf"
+    echo "%%~nf" | findstr /R /C:"development" >NUL
+    IF !ERRORLEVEL! EQU 0 (
+        echo Distributing APK %%~nxf to group development-testers...
+        firebase appdistribution:distribute "%%~dpnxf" --app %FIREBASE_PROJECT_ID% --groups development-testers --debug
     )
 
-    if not "!DISTRIBUTION_GROUP!"=="" (
-        echo Distributing APK %%~nxf to group %DISTRIBUTION_GROUP%...
-        firebase appdistribution:distribute "%%~dpnxf" --app %FIREBASE_PROJECT_ID% --groups %DISTRIBUTION_GROUP% --debug
+    echo "%%~nf" | findstr /R /C:"staging" >NUL
+    IF !ERRORLEVEL! EQU 0 (
+        echo Distributing APK %%~nxf to group staging-testers...
+        firebase appdistribution:distribute "%%~dpnxf" --app %FIREBASE_PROJECT_ID% --groups staging-testers --debug
     )
+
+    echo "%%~nf" | findstr /R /C:"production" > NUL
+    IF !ERRORLEVEL! EQU 0 (
+        echo Distributing APK %%~nxf to group production-testers...
+        firebase appdistribution:distribute "%%~dpnxf" --app %FIREBASE_PROJECT_ID% --groups production-testers --debug
+    )
+
 )
 
 echo All APKs distributed to Firebase!
+
+
