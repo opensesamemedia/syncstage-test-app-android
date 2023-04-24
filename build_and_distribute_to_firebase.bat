b4e3c1cbb187cf395c01e574440d1f198b034ee7
Patryk Kaczmarek <patryk.kaczmarek@wp.eu>
2023-04-24 12:02:08 +0200

    updated script for different distribution groups

--- build_and_distribute_to_firebase.bat
+++ build_and_distribute_to_firebase.bat
@@ -3,8 +3,11 @@ setlocal
 
 REM Define the Firebase project ID and distribution group ID
 set FIREBASE_PROJECT_ID=1:43735507988:android:f5f41e8de9650b4dc67301
-set DISTRIBUTION_GROUP_ID=internal
 
+REM Set the distribution group ID for each flavor
+set DEVELOPMENT_GROUP_ID=development-testers
+set STAGING_GROUP_ID=staging-testers
+set PRODUCTION_GROUP_ID=production-testers
 
 REM Loop through all project flavors and build the APKs
 for %%f in (development staging production) do (
@@ -15,8 +18,20 @@ for %%f in (development staging production) do (
 
 REM Loop through all built APKs and distribute them to Firebase
 for /R app\build\outputs\apk %%f in (*.apk) do (
-    echo Distributing APK %%~nxf...
-    firebase appdistribution:distribute "%%~dpnxf" --app %FIREBASE_PROJECT_ID% --groups %DISTRIBUTION_GROUP_ID% --debug
+    set DISTRIBUTION_GROUP=
+
+    if "%%~nf"=="app-development-debug" (
+        set DISTRIBUTION_GROUP=%DEVELOPMENT_GROUP_ID%
+    ) else if "%%~nf"=="app-staging-debug" (
+        set DISTRIBUTION_GROUP=%STAGING_GROUP_ID%
+    ) else if "%%~nf"=="app-production-debug" (
+        set DISTRIBUTION_GROUP=%PRODUCTION_GROUP_ID%
+    )
+
+    if not "%DISTRIBUTION_GROUP%"=="" (
+        echo Distributing APK %%~nxf to group %DISTRIBUTION_GROUP%...
+        firebase appdistribution:distribute "%%~dpnxf" --app %FIREBASE_PROJECT_ID% --groups %DISTRIBUTION_GROUP% --debug
+    )
 )
 
 echo All APKs distributed to Firebase!
