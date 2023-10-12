f92fb351144953debfa156152a97cba21987e2c3
ZygusPatryk <patryk.kaczmarek@wp.eu>
2023-10-12 12:53:49 +0200

    updated build scripts and enabled crashlitics

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ setlocal
 
 REM Loop through all project flavors and build the APKs
 REM for %%f in (development) do (
-for %%f in (development) do (
+for %%f in (development staging master) do (
     echo Building APK for %%f...
     gradlew.bat assemble%%fDebug
     REM gradlew.bat assemble%%fRelease
