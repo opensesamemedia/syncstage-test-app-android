20222ea0757ed4067c91588b18f129da56b772e5
Mateusz <mateusz28@gmail.com>
2023-11-24 10:19:21 +0100

    Fixed dot initial state

--- app/src/main/java/media/opensesame/syncstagetestappandroid/screens/SessionViewModel.kt
+++ app/src/main/java/media/opensesame/syncstagetestappandroid/screens/SessionViewModel.kt
@@ -39,7 +39,7 @@ data class ConnectionModel(
     val userId: String = "",
     val displayName: String? = "",
     var isMuted: Boolean = false,
-    var isConnected: Boolean = true,
+    var isConnected: Boolean = false,
     var volume: Float = 90.0f
 )
 
